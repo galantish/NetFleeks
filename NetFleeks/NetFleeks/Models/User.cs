7852ebedd798adbf2a821f37e9824542d90ee0a6
Bar Genish <genishbar@gmail.com>
2017-10-28 17:35:33 +0300

    user

---
+++ NetFleeks/NetFleeks/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetFleeks.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "First Name")]
+        public string fName { get; set; }
+        [Required]
+
+        [StringLength(255)]
+        [Display(Name = "Last Name")]
+        public string lName { get; set; }
+
+        [Display(Name = "Gender")]
+        public Gender gender { get; set; }
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public MembershipType membershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? birth { get; set; }
+
+        [Display(Name = "Favorite Genre")]
+        public int MyProperty { get; set; }
+
+    }
+}

adf3bdea00a8c84e82331967d144c92e91058626
Adi-Haviv <adi.haviv@gmail.com>
2017-10-28 17:31:26 +0300

    Purchase

--- NetFleeks/NetFleeks/Models/Purchase.cs
+++ NetFleeks/NetFleeks/Models/Purchase.cs
@@ -4,14 +4,11 @@ using System.Data.Entity;
 
 namespace NetFleeks.Models
 {
-    public class Genres
+    public class Purchase
     {
-        
         public int Id { get; set; };
-
-        [Required]
-        [StringLength(100)]
-        public string Name { get; set; };
-
+        public User User { get; set; };
+        public Movie Movie { get; set; };
+        public DateTime Expiration { get; set; };
     }
 }

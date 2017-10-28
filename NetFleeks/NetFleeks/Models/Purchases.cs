a95ec4d5168883defedf0e370ca634093ea52e5c
Adi-Haviv <adi.haviv@gmail.com>
2017-10-28 18:14:31 +0300

    Purchases

---
+++ NetFleeks/NetFleeks/Models/Purchases.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NetFleeks.Models
+{
+    public class Purchases
+    {
+        public int ID { get; set; }
+        public User purchaseUser { get; set; }
+        public Movies purchaseMovie { get; set; }
+        public DateTime purchaseExpiration { get; set; }
+    }
+}

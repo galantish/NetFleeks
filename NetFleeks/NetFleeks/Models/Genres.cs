878ef5e040643c683152e96a98f97d64b2a1e5c8
Adi-Haviv <adi.haviv@gmail.com>
2017-10-28 18:05:45 +0300

    Genres

---
+++ NetFleeks/NetFleeks/Models/Genres.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetFleeks.Models
+{
+    public class Genres
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string genreName { get; set; } 
+    }
+}

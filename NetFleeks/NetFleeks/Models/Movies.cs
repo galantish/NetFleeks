f19a326adfe1f6cdb03e60fdb11cf5cedf757fdc
shir.galanti <shir.galanti@BARUCHPE-LP-TB>
2017-10-28 17:46:57 +0300

    Movies Model Update

---
+++ NetFleeks/NetFleeks/Models/Movies.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NetFleeks.Models
+{
+    public class Movies
+    {
+        [Display(Name = "Movie ID")]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Movie Name")]
+        [StringLength(100)]
+        public string movieName { get; set; }
+
+       // [Required]
+        //[Display(Name = "Genre")]
+        //public Genre genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre ID")]
+        public int genreID { get; set; }
+
+        [Display(Name = "Date Added")]
+        public DateTime dateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime releaseDate { get; set; }
+
+        [Display(Name = "Actors")]
+        [StringLength(150)]
+        public string actors { get; set; }
+
+        [Display(Name = "Summary")]
+        [StringLength(500)]
+        public string summary { get; set; }
+    }
+}

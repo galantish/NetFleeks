b553b361d58ba1ea14ac06e630266b59c73ffc88
Adi-Haviv <adi.haviv@gmail.com>
2017-10-28 18:24:10 +0300

    s

---
+++ NetFleeks/NetFleeks/Models/Rentals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetFleeks.Models
+{
+    public class Rentals
+    {
+        public int ID { get; set; }
+        [Display(Name = "Renting User")]
+        public User rentalUser { get; set; }
+        [Display(Name = "Rented Movie")]
+        public Movies rentalMovie { get; set; }
+        [Display(Name = "Rental Expiration Date")]
+        public DateTime rentalExpiration { get; set; }
+    }
+}

843b973f810d7114889467b009c6c108dea4edd3
shir.galanti <shir.galanti@BARUCHPE-LP-TB>
2017-11-04 10:46:24 +0200

    Insert data to DB

---
+++ NetFleeks/NetFleeks/Migrations/dbo.Genres.data.sql
@@ -0,0 +1,12 @@
+﻿SET IDENTITY_INSERT [dbo].[Genres] ON
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (1, N'Comedy')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (2, N'Romance')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (3, N'Drama')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (4, N'Action')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (5, N'Horror')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (6, N'SciFi')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (7, N'Kids')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (8, N'Family')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (9, N'Biography')
+INSERT INTO [dbo].[Genres] ([ID], [genreName]) VALUES (10, N'Adult')
+SET IDENTITY_INSERT [dbo].[Genres] OFF

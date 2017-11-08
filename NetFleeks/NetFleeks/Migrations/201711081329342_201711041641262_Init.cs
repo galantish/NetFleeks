44b17051a1340f847bb75fa3c0bfcb38bda9e25c
shir.galanti <shir.galanti@BARUCHPE-LP-TB>
2017-11-08 17:59:49 +0200

    GENISH MY LIFE 2

---
+++ NetFleeks/NetFleeks/Migrations/201711081329342_201711041641262_Init.cs
@@ -0,0 +1,16 @@
+namespace NetFleeks.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _201711041641262_Init : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

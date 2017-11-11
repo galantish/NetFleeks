bbc3fc35f0cfe0badd6ea2b081b1c2fa09c31abf
Bar Genish <genishbar@gmail.com>
2017-11-11 19:45:42 +0200

    GroupBy

---
+++ NetFleeks/NetFleeks/ViewModel/RentalViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NetFleeks.ViewModel
+{
+    public class RentalViewModel
+    {
+        [Display(Name = "Genre Name")]
+        public string genre { get; set; }
+
+        [Display(Name = "Movie Name")]
+        public string movie { get; set; }
+
+        [Display(Name = "Rental Expiration")]
+        public DateTime rentalExpiration { get; set; }
+
+        [Display(Name = "User rented")]
+        public string rentalUser { get; set; }
+    }
+}

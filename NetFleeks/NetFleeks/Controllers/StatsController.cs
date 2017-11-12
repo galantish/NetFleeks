2130429ec06b77235860bace181007c77f55b3dc
Bar Genish <genishbar@gmail.com>
2017-11-12 23:12:47 +0200

    Stats

---
+++ NetFleeks/NetFleeks/Controllers/StatsController.cs
@@ -0,0 +1,55 @@
+﻿using NetFleeks.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetFleeks.Models
+{
+    public class StatsController : Controller
+    {
+      
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Stats
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GenreCount()
+        {
+            var rentals = db.Rentals;
+            var rentalsGenre = rentals.Join(db.Movies, r => r.rentalMovie, m => m.movieName, (r, m) => new RentalViewModel { movie = r.rentalMovie, genre = m.genre.genreName, rentalUser = r.rentalUser, rentalExpiration = r.rentalExpiration });
+            var pieGenreCollection = rentalsGenre.GroupBy(t => t.genre).Select(i => new { genre = i.Key, Count = i.Count() });
+            StringBuilder genreBuilder = new StringBuilder();
+
+            genreBuilder.AppendLine("genre,count");
+
+            foreach (var currGenre in pieGenreCollection)
+            {
+                genreBuilder.AppendFormat("{0},{1}\\n", currGenre.genre, currGenre.Count);
+            }
+            Stats stats = new Stats() { genreCsv = genreBuilder.ToString() };
+            return View("Index", stats);
+
+        }
+
+        public ActionResult TypeCount()
+        {
+
+            var userGroups = db.Users.GroupBy(x => x.membershipTypeID).Select(x => new { ID = x.Key, Count = x.Count() });
+
+            int premiumCount = userGroups.Single(x => x.ID == db.MembershipTypes.FirstOrDefault(mem => mem.membershipType.Equals("Premium")).ID).Count;
+            int freeCount = userGroups.Single(x => x.ID == db.MembershipTypes.FirstOrDefault(mem => mem.membershipType.Equals("Free")).ID).Count;
+
+            string memTypesCSV = string.Format("type,count\\nPremium,{0}\\nFree,{1}", premiumCount, freeCount);
+
+            Stats stats = new Stats() { memTypeCsv = memTypesCSV };
+            return View("TypeCount", stats);
+
+        }
+    }
+}
---
+++ NetFleeks/NetFleeks/Models/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetFleeks.Models
+{
+    public class Stats
+    {
+        public string memTypeCsv { get; set; }
+        public string genreCsv { get; set; }
+    }
+}
